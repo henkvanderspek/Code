b22ee0804c3431e6bccb281f8c4f5abbfb0dc702
Henk van der Spek <henk@vanmezelf.nl>
2022-02-24 14:59:48 +0100

    Initial Commit

---
+++ Code/CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeApp.swift
+//  Code
+//
+//  Created by Henk van der Spek on 24/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Code
+//
+//  Created by Henk van der Spek on 24/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
