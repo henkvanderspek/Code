a06fea57980b4db578c75ab537b9e24e24acce93
Henk van der Spek <henk@vanmezelf.nl>
2022-03-20 14:27:42 +0100

    Compose embed view WIP

---
+++ Shared/Common/CameraView.swift
@@ -0,0 +1,75 @@
+//
+//  CameraView.swift
+//  Code
+//
+//  Created by Henk van der Spek on 20/03/2022.
+//
+
+import UIKit
+import AVKit
+
+class CameraView: UIView {
+
+    private lazy var videoDataOutput: AVCaptureVideoDataOutput = {
+        let v = AVCaptureVideoDataOutput()
+        v.alwaysDiscardsLateVideoFrames = true
+        v.connection(with: .video)?.isEnabled = true
+        v.setSampleBufferDelegate(self, queue: .main)
+        return v
+    }()
+
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let l = AVCaptureVideoPreviewLayer(session: session)
+        l.videoGravity = .resizeAspectFill
+        return l
+    }()
+
+    private lazy var captureDevice: AVCaptureDevice? = {
+        AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: position)
+    }()
+    
+    private lazy var session: AVCaptureSession = {
+        let s = AVCaptureSession()
+        s.sessionPreset = .vga640x480
+        return s
+    }()
+    
+    private let position: AVCaptureDevice.Position
+    private var image: CIImage?
+    
+    private lazy var context = {
+        CIContext()
+    }()
+
+    init(frame: CGRect = .zero, position p: AVCaptureDevice.Position) {
+        position = p
+        super.init(frame: frame)
+        layer.masksToBounds = true
+        layer.addSublayer(previewLayer)
+        DispatchQueue.main.async(execute: beginSession)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func beginSession() {
+        guard let d = captureDevice, let i = try? AVCaptureDeviceInput(device: d) else { return }
+        session.addInput(i)
+        session.addOutput(videoDataOutput)
+        session.startRunning()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        previewLayer.frame = bounds
+    }
+}
+
+extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        connection.videoOrientation = .portraitUpsideDown
+        image = CIImage(cvPixelBuffer: pixelBuffer)
+    }
+}
--- Shared/Views/ComposeTextView.swift
+++ Shared/Views/ComposeTextView.swift
@@ -24,17 +24,17 @@ struct TextView: UIViewRepresentable {
     private let coordinator: InnerView
     init(text: Binding<String>) {
         coordinator = InnerView(value: text)
-    }
-    func makeCoordinator() -> InnerView {
         coordinator.paragraphSpacing = 32
         coordinator.imageContainerHeight = 300
+    }
+    func makeCoordinator() -> InnerView {
         return coordinator
     }
     func makeUIView(context: Context) -> UITextView {
         let v = context.coordinator
         v.autocorrectionType = .no
         v.spellCheckingType = .no
-        v.textContainerInset = .all(16)
+        v.textContainerInset = .vertical(16)
         v.becomeFirstResponder()
         return v
     }
@@ -54,19 +54,21 @@ extension TextView {
         @Binding var value: String
         var paragraphSpacing: CGFloat = 0.0
         var imageContainerHeight: CGFloat = 0.0
+        private let uti = "public.text"
         init(value v: Binding<String>) {
             _value = v
             super.init(frame: .zero, textContainer: nil)
             delegate = self
             layoutManager.delegate = self
+            textContainer.lineFragmentPadding = 0
+            NSTextAttachment.registerViewProviderClass(ImageViewProvider.self, forFileType: uti)
         }
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
         override func caretRect(for position: UITextPosition) -> CGRect {
             let r = super.caretRect(for: position)
-            print(r.height)
-            print(textRange(from: position, to: position))
+            // TODO: Fix magic numbers
             let height = r.height < imageContainerHeight ? ((typingFont?.lineHeight ?? 0.0) + 2.0).rounded(.toNearestOrEven) + 0.5 : r.height
             return .init(
                 origin: r.origin,
@@ -79,34 +81,71 @@ extension TextView {
     }
 }
 
-extension TextView.InnerView: NSLayoutManagerDelegate {
-    func layoutManager(_ layoutManager: NSLayoutManager, paragraphSpacingAfterGlyphAt glyphIndex: Int, withProposedLineFragmentRect rect: CGRect) -> CGFloat {
-        typingFont = .preferredFont(forTextStyle: .body).withDesign(.serif)
-        return paragraphSpacing
-    }
-}
+extension TextView.InnerView: NSLayoutManagerDelegate {}
 
 extension TextView.InnerView: UITextViewDelegate {
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         value = text
         if text == "\n" {
-            let w = bounds.width - textContainerInset.left - textContainerInset.right
-            let a = NSTextAttachment()
-            a.image = .init(systemName: "mustache")!
-            a.bounds = .init(origin: .zero, size: .init(width: w, height: imageContainerHeight))
-            let s = NSMutableAttributedString(attachment: a)
-            textStorage.append(s)
-        } else {
-            print(text)
+            let a = NSTextAttachment(data: nil, ofType: uti)
+            a.bounds = .init(
+                origin: .zero,
+                size: .init(
+                    width: bounds.width,
+                    height: imageContainerHeight
+                )
+            )
+            let t = attributedText.mutableCopy() as! NSMutableAttributedString
+            let loc = t.length + 1
+            t.append(.init(string: "\n"))
+            t.append(.init(attachment: a))
+            t.addAttributes([
+                    .font: UIFont.preferredFont(forTextStyle: .body).withDesign(.serif),
+                    .backgroundColor: UIColor.red,
+                    .paragraphStyle: NSParagraphStyle.custom(spacing: paragraphSpacing, indent: 0)
+                ],
+                range: .init(location: loc, length: t.length - loc)
+            )
+            attributedText = t
+            //print(attributedText)
+            return false
         }
         return true
     }
 }
 
 extension TextView.InnerView {
+    class ImageViewProvider: NSTextAttachmentViewProvider {
+        override func loadView() {
+//            let label = UILabel()
+//            label.text = "Test"
+//            label.textAlignment = .center
+//            view = label
+            view = CameraView(position: .front)
+        }
+    }
+}
+
+extension NSParagraphStyle {
+    static func custom(spacing: CGFloat, indent: CGFloat = 16.0) -> NSParagraphStyle {
+        let p = NSMutableParagraphStyle()
+        p.setParagraphStyle(.default)
+        p.paragraphSpacing = spacing / 2
+        p.paragraphSpacingBefore = spacing / 2
+        p.firstLineHeadIndent = indent
+        p.headIndent = indent
+        p.tailIndent = -indent
+        return p
+    }
+}
+
+extension TextView.InnerView {
     var typingFont: UIFont? {
         set {
-            typingAttributes = typingAttributes.merging([.font: newValue as Any], uniquingKeysWith: { lhs, rhs in return rhs })
+            var a = typingAttributes
+            a[.font] = newValue
+            a[.paragraphStyle] = NSParagraphStyle.custom(spacing: paragraphSpacing)
+            typingAttributes = a
         }
         get {
             return typingAttributes[.font] as? UIFont
