95cb491f50149538b065c978797e6719f93379ea
Henk van der Spek <henk@vanmezelf.nl>
2022-05-27 22:22:28 +0200

    Removed all storage code

--- macOS/Types/StorageControlling.swift
+++ macOS/Types/StorageControlling.swift
@@ -131,28 +131,3 @@ extension NSEntityDescription {
         )
     }
 }
-
-//
-//protocol AppStoring {
-//    func store(_ a: Uicorn.App)
-//}
-//
-//class LocalAppStorage: AppStoring {
-//    private lazy var pasteboard: NSPasteboard = {
-//        return .general
-//    }()
-//    private lazy var encoder: JSONEncoder = {
-//        return .init()
-//    }()
-//    func store(_ a: Uicorn.App) {
-//        do {
-//            let d = try encoder.encode(a)
-//            let s = String(data: d, encoding: .utf8)!
-//            print(s)
-//            pasteboard.clearContents()
-//            pasteboard.setString(s, forType: .string)
-//        } catch {
-//            print(error)
-//        }
-//    }
-//}
